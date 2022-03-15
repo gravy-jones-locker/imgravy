beb43441e007aec44926de82cd749af595fcc216
gravy-jones-locker <gravy@gravyjoneslocker.uk>
2022-03-15 14:32:10 +0000

    Working commit

--- imgravy/image.py
+++ imgravy/image.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Union
+
 import numpy as np
 import cv2
 
@@ -13,14 +15,14 @@ class Image:
     class Decorators:
 
         @classmethod
-        def supply_new(decs: Decorators, func: function):
+        def supply_new(decs, func: function):
             """Perform the specified op on a new (copy) Image object"""
             def inner(im: Image, *args, **kwargs):
                 return func(type(im)(im.arr.copy()), *args, **kwargs)
             return inner
 
         @classmethod
-        def prep_8UC1(decs: Decorators,  func: function):
+        def prep_8UC1(decs,  func: function):
             """Supply a rounded version of the image with appropriate dtype"""
             @decs.supply_new
             def inner(im: Image, *args, **kwargs):
@@ -29,20 +31,29 @@ class Image:
             return inner
         
         @classmethod
-        def return_res(decs: Decorators,  func: function):
+        def return_res(decs,  func: function):
             """Replies the altered Image rather than function output"""
             def inner(im: Image, *args, **kwargs):
                 func(im, *args, **kwargs)
                 return im
             return inner
 
-    def __init__(self, in_arr: np.array):
-        self.arr = in_arr.copy()  # Bind copy for hygiene
+    def __init__(self, in_obj: Union[str, bytes, np.ndarray]) -> None:
+        """Execute correct method to bind array from the input object"""
+        if isinstance(in_obj, np.ndarray):
+            self.arr = in_obj
+        if isinstance(in_obj, str):
+            self.arr = self._parse_file(in_obj)
+        if isinstance(in_obj, bytes):
+            self.arr = self._parse_bytes(in_obj)
         
-    @classmethod
-    def _from_bytes(cls, stream: bytes) -> Image:
+    def _parse_bytes(self, stream: bytes) -> Image:
         """Load an Image class from a bytes stream"""
-        return cls(cv2.imdecode(np.frombuffer(stream, np.uint8), flags=1))
+        return cv2.imdecode(np.frombuffer(stream, np.uint8), flags=1)
+    
+    def _parse_file(self, fpath: str) -> Image:
+        """Load an Image class from a file path"""
+        return cv2.imread(fpath)
     
     def _to_bytes(self, ftype: str) -> bytes:
         """Convert an image array to a stream of bytes"""
@@ -66,11 +77,23 @@ class Image:
         M[0, 2] += x_off
         M[1, 2] += y_off
         self.arr = cv2.warpAffine(self.arr, M, (self.w, self.h))
+    
+    @Decorators.return_res
+    def annotate(self, **annotations) -> None:
+        """Draws on boxes/lines/contours/text/polys as specified"""
+        if len(self.arr.shape) == 2:
+            self.cvt2color()  # Guarantees annotations will show
+        for key, value in annotations.items():
+            eval(f'self.draw_{key}(value)')
 
     @Decorators.return_res
     def cast(self, casttype: str) -> None:
         """Cast the image array to the type specified"""
         self.arr = self.arr.astype(casttype)
+    
+    def copy(self) -> Image:
+        """Provide a copy of the Image object for unconnected edits"""
+        return Image(self.arr.copy())
 
     @Decorators.return_res
     def crop(self, pt0: tuple, pt1: tuple) -> None:
@@ -93,14 +116,21 @@ class Image:
     @Decorators.return_res
     def cvt8UC1(self) -> None:
         """Converts into filter-friendly format (i.e. unsigned 0-255)"""
-        self.norm_minmax().cast('uint8')
+        self.cast('uint8')
 
     @Decorators.return_res
     def cvt2color(self) -> None:
         """Convert a greyscale image to a color image"""
-        if len(self.arr.shape) == 2:
-            raise Exception('Can\'t convert from color -> color')
+        if len(self.arr.shape) == 3:
+            return  # True if already color
         self.arr = cv2.merge([self.arr, self.arr, self.arr])
+    
+    @Decorators.return_res
+    def cvt2gray(self) -> None:
+        """Convert a color image to a grayscale image"""
+        if len(self.arr.shape) == 2:
+            return  # True if already grayscale
+        self.arr = cv2.cvtColor(self.arr, cv2.COLOR_BGR2GRAY)
 
     @Decorators.return_res
     def draw_boxes(self, boxes: list, fill_type: int=1, 
@@ -116,15 +146,14 @@ class Image:
         cv2.drawContours(self.arr, cnts, -1, color, fill_type)
 
     @Decorators.return_res
-    def draw_lines(self, lines: list, fill_type: int=1,
+    def draw_lines(self, lines: list, width: int=1,
     color: tuple=(0, 0, 255)) -> None:
         """Draw lines onto an image"""
         for pt1, pt2 in lines:
-            cv2.line(self.arr, pt1, pt2, color, fill_type, cv2.LINE_AA)
+            cv2.line(self.arr, pt1, pt2, color, width, cv2.LINE_AA)
 
     @Decorators.return_res
-    def draw_polys(self, polys: list, fill_type: int=1,
-    color: tuple=(0, 0, 255)) -> None:
+    def draw_polys(self, polys: list, color: tuple=(0, 0, 255)) -> None:
         """Draw a (closed) polygon onto an image"""
         for pts in polys:
             lines = []
@@ -132,6 +161,29 @@ class Image:
                 pt1 = pts[i+1] if i < len(pts) - 1 else pts[0]
                 lines.append((pt0[0], pt1[0]))
             self.draw_lines(lines, color=color) 
+
+    @Decorators.return_res
+    def draw_text(self, text: str, position: tuple, size: int, 
+    color: Union[int, tuple]=255) -> None:
+        """Shorthand for cv2 putText method"""
+        cv2.putText(self.arr,
+                    str(text),
+                    position,
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    size,
+                    color,
+                    1)
+    
+    @Decorators.return_res
+    def fit_window(self) -> None:
+        """Resize to the max dim given in DEBUG_SIZE_PX"""
+        if self.h > self.w:
+            ratio = DEBUG_SIZE_PX / self.h
+            DEBUG_DIMS = int(self.w * ratio), DEBUG_SIZE_PX
+        else:
+            ratio = DEBUG_SIZE_PX / self.w
+            DEBUG_DIMS = DEBUG_SIZE_PX, int(self.h * ratio)
+        self.resize(*DEBUG_DIMS)
 
     @Decorators.return_res  # TODO I don't know what this does
     def flood_fill(self, c: list, color: tuple=(0, 0, 255)) -> None:
@@ -185,14 +237,20 @@ class Image:
                                     cv2.BORDER_DEFAULT)
 
     @Decorators.return_res
-    def norm_minmax(self, lo: int=None, hi: int=None) -> None:
+    def norm_minmax(self, lo: int=None, hi: int=None, bound: bool=False, 
+    keep_dtype: bool=False) -> None:
         """Normalize such that the specified values are set to 0/255"""
-        if hi is None:
-            hi = self.hi  # Set min/max to limits of image if None passed
+        if hi is None:  # Set min/max to limits of image if None passed
+            hi = self.hi  
         if lo is None:
             lo = self.lo
-        self.arr = self.arr - lo
-        self.arr = self.arr * 255 / (hi - lo)
+        dtype = self.arr.dtype
+        self.arr = (self.cast('float64').arr - lo) * (255 / (hi - lo))
+        if bound:
+            self.arr[self.arr > 255] = 255
+            self.arr[self.arr < 0] = 0
+        if keep_dtype and dtype != self.arr.dtype:
+            self.arr = self.cast(dtype)
 
     @Decorators.return_res
     def pad(self, hi: int, lo: int, l: int, r: int,
@@ -208,13 +266,6 @@ class Image:
         cv2.imwrite(fpath, self.arr)
 
     @Decorators.return_res
-    def put_text(self, text: str, size: int, 
-    color: Union[int, tuple]=255) -> None:
-        """Shorthand for cv2 puttext method"""
-        cv2.putText(
-        self.arr, str(text), (2, 10), cv2.FONT_HERSHEY_SIMPLEX, size, color, 1)
-
-    @Decorators.return_res
     def resize(self, new_w: int=None, new_h: int=None) -> None:
         """Resize the image to the dimensions specified"""
         self.arr = cv2.resize(self.arr, (new_w if new_w else self.w, 
@@ -224,8 +275,7 @@ class Image:
         """Spin the image through the angle specified"""
         M = cv2.getRotationMatrix2D(self.c, theta, 1)
         if bound:
-            # TODO I'm not sure what this does either
-            cos, sin = np.abs(M[0])
+            cos, sin = np.abs(M[0, :2])
             
             w = int((self.h * sin) + (self.w * cos))
             h = int((self.h * cos) + (self.w * sin))
@@ -258,47 +308,34 @@ class Image:
         
         return self
 
-    def slice_rng(self, lo=None, hi=None):
-        """
-        Slice the intensities in the image to those inside the range specified.
-        """
-        if lo:
+    @Decorators.return_res
+    def slice_rng(self, lo=None, hi=None) -> None:
+        """Slice the intensities in the image to those inside the range"""
+        if lo is not None:
             self.arr[self.arr < lo] = self.lo
-        if hi:
+        if hi is not None:
             self.arr[self.arr > hi] = self.hi
 
-        return self
-
     @Decorators.prep_8UC1
-    def show(self, cnts=[], boxes=[], text='', lines=[], polys=[]):
-        """
-        Normalize and show the image for debugging purposes.
-        """
-        # Operations will have no effect if no objects supplied
-        if len(self.arr.shape) == 2:
-            self.cvt2color()
-        self.draw_contours(cnts).draw_boxes(boxes).draw_lines(lines)
-        self.put_text(text).draw_polys(polys)
-
-        if self.h > self.w:
-            ratio = DEBUG_SIZE_PX / self.h
-            DEBUG_DIMS = int(self.w * ratio), DEBUG_SIZE_PX
-        else:
-            ratio = DEBUG_SIZE_PX / self.w
-            DEBUG_DIMS = DEBUG_SIZE_PX, int(self.h * ratio)
-        
-        self.resize(*DEBUG_DIMS)
+    def show(self, resize: bool=True, **annotations) -> None:
+        """Annotate/normalize the image"""
+        self.annotate(**annotations)
+        if resize:
+            self.fit_window()
         cv2.imshow('progress', self.arr)
-        if cv2.waitKey(0) == ord('q'):
-            cv2.destroyAllWindows()
+        while cv2.waitKey(0) != ord('q'):
+            pass
+        cv2.destroyAllWindows()
 
+    @Decorators.return_res
     def threshold(self, thresh_val):
         """
         Apply binary thresholding using the value specified.
         """
-        _, self.arr = cv2.threshold(self.arr, thresh_val, 255,
-                                                            cv2.THRESH_BINARY)
-        return self
+        _, self.arr = cv2.threshold(self.arr,
+                                    thresh_val,
+                                    255,
+                                    cv2.THRESH_BINARY)
     
     ####   Expensive properties evaluated as private functions   ####
 
@@ -312,11 +349,11 @@ class Image:
 
     @property
     def h(self):
-        return self.size[0]
+        return self.arr.shape[0]
     
     @property
     def w(self):
-        return self.size[1]
+        return self.arr.shape[1]
 
     @property
     def area(self):
